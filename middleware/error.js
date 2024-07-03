cab2ed5d86951da00712390630ac01c98aa23a26
Salah eddine ettounsi <salaheddineettounsi@gmail.com>
2024-07-03 00:34:57 -0400

    some minor changes

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,8 @@
 // TODO: Custom Error Handler to be finished
 const errorHandler = (err, req, res, next) => {
+  console.log(err.name);
+  console.log(err.message.red);
+  console.log(err.stack.red);
   res.status(err.statusCode || 500).json({
     success: false,
     error: err.message || "Server Error",

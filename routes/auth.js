e818d9bb14ba6829d7fb373fdf8ee65878579490
HusseinCoulibaly <GnenemaCoulibaly@teccart.online>
2024-08-15 16:14:47 -0400

    Refactor of server
    Co-authored-by: Salah-Edd. Ett. <anyone2k@users.noreply.github.com>

--- routes/auth.js
+++ routes/auth.js
@@ -10,17 +10,21 @@ const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router();
 
+// login & register for doctors
+
+router.route("/doctor/login");
+router.route("/doctor/register");
+router.route("/doctor/refresh-token");
+
+// login & register for patient
 router.route("/patient/login").post(postLogin);
-
 router.route("/patient/register").post(postRegister);
-
 router.route("/patient/refresh-token").get(refreshAccessToken);
 
 // make routes for staff
 router.route("/staff/login").post(postLogin);
-
 router.route("/staff/register").post(protect, authorize("admin"), postRegister);
-
 router.route("/staff/refresh-token").get(refreshAccessToken);
+
 
 module.exports = router;

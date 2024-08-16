62e65151a568ae171d728d00b9c4fd15d44b0826
anyone2k <Salaheddineettounsi@gmail.com>
2024-08-15 21:03:55 -0400

    chore: Refactor auth middleware to support different user types

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,50 +4,53 @@ const jwt = require("jsonwebtoken");
 // Internal imports
 const Staff = require("../Models/Staff");
 
-const Patient = require("../models/Patient");
+const Patient = require("../Models/Patient");
 
+const Doctor = require("../Models/Doctor");
 const asyncHandler = require("./async");
 
-exports.protect = async (req, res, next) => {
-  try {
-    let token = req.headers.authorization;
+exports.protect = (user_type) => {
+  return async (req, res, next) => {
+    let user;
+    try {
+      let token = req.headers.authorization;
 
-    if (token === undefined) {
+      if (token === undefined) {
+        return res
+          .status(401)
+          .send("Not authorized to access this route / Invalid Token");
+      }
+
+      if (!token.startsWith("Bearer")) {
+        return res
+          .status(401)
+          .send("Not authorized to access this route / Invalid Token");
+      }
+
+      token = token.split(" ")[1];
+      const tokenDecoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+      if (user_type === "staff") {
+        user = await Staff.findOne({ _id: tokenDecoded._id });
+      } else if (user_type === "patient") {
+        user = await Patient.findOne({ _id: tokenDecoded._id });
+      } else {
+        user = await Doctor.findOne({ _id: tokenDecoded._id });
+      }
+
+      if (!user) {
+        return res
+          .status(401)
+          .send("Not authorized to access this route / Invalid Token");
+      }
+      req.user = user;
+      next();
+    } catch (error) {
       return res
         .status(401)
         .send("Not authorized to access this route / Invalid Token");
     }
-
-    if (!token.startsWith("Bearer")) {
-      return res
-        .status(401)
-        .send("Not authorized to access this route / Invalid Token");
-    }
-
-    token = token.split(" ")[1];
-    const tokenDecoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    const staff = await Staff.findOne({ _id: tokenDecoded._id });
-    const patient = await Patient.findOne({ _id: tokenDecoded._id });
-
-    if (!staff && !patient) {
-      return res
-        .status(401)
-        .send("Not authorized to access this route / Invalid Token");
-    }
-
-    if (staff) {
-      req.user = staff;
-    } else {
-      req.user = patient;
-    }
-
-    next();
-  } catch (error) {
-    return res
-      .status(401)
-      .send("Not authorized to access this route / Invalid Token");
-  }
+  };
 };
 
 exports.isDoctor = asyncHandler(async (req, res, next) => {

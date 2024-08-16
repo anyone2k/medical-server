17636256bf7c683b0391eaa7d078aad1c946b23b
HusseinCoulibaly <GnenemaCoulibaly@teccart.online>
2024-08-15 21:16:45 -0400

    protect

--- routes/auth.js
+++ routes/auth.js
@@ -19,21 +19,20 @@ const router = express.Router();
 // login & register for doctors
 
 router.route("/doctor/login").post(postDoctorLogin);
-router
-  .route("/doctor/register")
-  .post(protect, authorize("admin"), postDoctorRegister);
+router .route("/doctor/register").post(protect("staff"), authorize("admin"), postDoctorRegister);
+  
 router.route("/doctor/refresh-token").post(doctorRefreshAccessToken);
 
 // login & register for patient
 router.route("/patient/login").post(postPatientLogin);
-router.route("/patient/register").post(protect, authorize("admin"),postPatientRegister);
+router.route("/patient/register").post(protect("staff"), authorize("admin"),postPatientRegister);
 router.route("/patient/refresh-token").get(patientRefreshAccessToken);
 
 // make routes for staff
 router.route("/staff/login").post(postStaffLogin);
 router
   .route("/staff/register")
-  .post(protect, authorize("admin"), postStaffRegister);
+  .post(protect("staff"), authorize("admin"), postStaffRegister);
 router.route("/staff/refresh-token").get(staffRefreshAccessToken);
 
 module.exports = router;
